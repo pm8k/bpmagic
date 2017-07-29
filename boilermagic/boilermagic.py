e2c52aa40549716e671749251fb3547f91c3705d
pm8k <astromars42@gmail.com>
2017-07-28 22:22:14 -0500

    restructed repo and added stuff for pypi

--- boilermagic/boilermagic.py
+++ boilermagic/boilermagic.py
@@ -1,46 +1,49 @@
 from IPython.core.magic import (Magics, magics_class, line_magic,
                                 cell_magic)
 from IPython.core.magic_arguments import (argument, magic_arguments,
-    parse_argstring)
+                                          parse_argstring)
 import os
-import jupyter
+
+
 def check_directory():
     userpath = os.path.expanduser('~')
     dirpath = "{UP}/.boilermagic".format(UP=userpath)
     if not os.path.isdir(dirpath):
         os.mkdir(dirpath)
 
-# Main Magic Class 
+# Main Magic Class
+
+
 @magics_class
 class BoilerMagic(Magics):
-    
+
     # function to load one or more modules into an ipython cell
     @magic_arguments()
-    @argument('arg',nargs='*',action='append')
+    @argument('arg', nargs='*', action='append')
     @line_magic
     def iload(self, arg):
         args = parse_argstring(self.iload, arg).arg[0]
-        
+
         if len(args) == 0:
             ValueError('No arguments passed to %iload')
         linelist = []
         userpath = os.path.expanduser('~')
         dirpath = "{UP}/.boilermagic".format(UP=userpath)
         for arg in args:
-            filepath = "{DP}/{PROF}.py".format(PROF=arg,DP=dirpath)
+            filepath = "{DP}/{PROF}.py".format(PROF=arg, DP=dirpath)
             if not os.path.isfile(filepath):
-                raise ValueError('No boilermagic file named {PROF}. Use %ilist to list all profiles.'.format(PROF=arg))
-            file = open(filepath,'r')
+                raise ValueError(
+                    'No boilermagic file named {PROF}. Use %ilist to list all profiles.'.format(PROF=arg))
+            file = open(filepath, 'r')
             lines = file.read()
-            lines = '#'+arg+'\n'+lines
+            lines = '#' + arg + '\n' + lines
             linelist.append(lines)
             file.close()
         alllines = '\n\n'.join(linelist)
         self.shell.set_next_input(alllines, replace=True)
 
-        #self.shell.ex(alllines)
+        # self.shell.ex(alllines)
 
-        
     # load all saved chunks into a single cell
     # anyway to load 'groups' of cells? any use?
     @line_magic
@@ -48,51 +51,52 @@ class BoilerMagic(Magics):
         userpath = os.path.expanduser('~')
         dirpath = "{UP}/.boilermagic".format(UP=userpath)
         names = os.listdir(dirpath)
-        
+
         if len(names) == 0:
             ValueError('No profiles to load, save profiles first using %isave')
         linelist = []
         for arg in names:
-            filepath = "{DP}/{PROF}.py".format(PROF=arg,DP=dirpath)
+            filepath = "{DP}/{PROF}.py".format(PROF=arg, DP=dirpath)
 
-            file = open(filepath,'r')
+            file = open(filepath, 'r')
             lines = file.read()
-            lines = '#'+arg+'\n'+lines
+            lines = '#' + arg + '\n' + lines
             linelist.append(lines)
             file.close()
         alllines = '\n\n'.join(linelist)
         self.shell.set_next_input(alllines, replace=True)
-        
+
     # save everything in the current cell to the name of the profile
     # if overwriting a previously defined profile use -o or --overwrite
     @magic_arguments()
-    @argument('profile',nargs=1,action='append')
-    @argument('--overwrite','-o',action='store_true')
+    @argument('profile', nargs=1, action='append')
+    @argument('--overwrite', '-o', action='store_true')
     @cell_magic
-    def isave(self,line, cell = None, local_ns = None):
-        
+    def isave(self, line, cell=None, local_ns=None):
+
         # ensure there is a .imports dire
-        check_directory()        
+        check_directory()
         args = parse_argstring(self.isave, line)
         profile = args.profile[0][0]
-        #TODO: Check charstring of filename to see if it can be written 
+        # TODO: Check charstring of filename to see if it can be written
         overwrite = args.overwrite
         userpath = os.path.expanduser('~')
         dirpath = "{UP}/.boilermagic".format(UP=userpath)
-        filepath = "{DP}/{PROF}.py".format(PROF=profile,DP=dirpath)
+        filepath = "{DP}/{PROF}.py".format(PROF=profile, DP=dirpath)
 
         if overwrite is not True:
             if os.path.isfile(filepath):
                 print 'here'
-                raise ValueError('Already Exists, use -o or --overwrite to overwrite file')
-        file = open(filepath,"w") 
+                raise ValueError(
+                    'Already Exists, use -o or --overwrite to overwrite file')
+        file = open(filepath, "w")
         file.write(cell)
         file.close()
 
     # list every profile currently saved
     @line_magic
-    def ilist(self,line):
-        
+    def ilist(self, line):
+
         userpath = os.path.expanduser('~')
         dirpath = "{UP}/.boilermagic".format(UP=userpath)
 
@@ -100,60 +104,61 @@ class BoilerMagic(Magics):
         names = [x[:-3] for x in names]
         for n in names:
             print n
-            
-     #TODO: See if you can add run functionality to iload
-        
+
+    # TODO: See if you can add run functionality to iload
+
     # delete the named profile
     @magic_arguments()
-    @argument('arg',nargs='*',action='append')
+    @argument('arg', nargs='*', action='append')
     @line_magic
     def idelete(self, arg):
         args = parse_argstring(self.idelete, arg).arg[0]
-        
+
         if len(args) == 0:
             ValueError('No arguments passed to %idelete')
         userpath = os.path.expanduser('~')
         dirpath = "{UP}/.boilermagic".format(UP=userpath)
         filepath = "{DP}/{PROF}.py".format(PROF=arg, DP=dirpath)
         if not os.path.isfile(filepath):
-            raise ValueError('No boilermagic file named {PROF}. Use %ilist to list all profiles.'.format(PROF=arg))
+            raise ValueError(
+                'No boilermagic file named {PROF}. Use %ilist to list all profiles.'.format(PROF=arg))
         else:
             os.remove(filepath)
             print 'Profile {PROF} has been deleted'.format(PROF=arg)
-            
-            
+
     # rename profile from one name to another
     @magic_arguments()
-    @argument('oldprofile',nargs=1,action='append')
-    @argument('newprofile',nargs=1,action='append')
-    @argument('--overwrite','-o',action='store_true')
+    @argument('oldprofile', nargs=1, action='append')
+    @argument('newprofile', nargs=1, action='append')
+    @argument('--overwrite', '-o', action='store_true')
     @line_magic
-    def irename(self,line):
-        
+    def irename(self, line):
+
         # ensure there is a .imports dire
-        check_directory()        
+        check_directory()
         args = parse_argstring(self.irename, line)
         userpath = os.path.expanduser('~')
         dirpath = "{UP}/.boilermagic".format(UP=userpath)
         oldprofile = args.oldprofile[0][0]
         newprofile = args.newprofile[0][0]
-        
+
         oldfilepath = "{DP}/{PROF}.py".format(PROF=oldprofile, DP=dirpath)
         if not os.path.isfile(oldfilepath):
-            raise ValueError('No boilermagic file named {PROF}. Use %ilist to list all profiles.'.format(PROF=arg))
-        
-        #TODO: Check charstring of filename to see if it can be written 
+            raise ValueError(
+                'No boilermagic file named {PROF}. Use %ilist to list all profiles.'.format(PROF=arg))
+
+        # TODO: Check charstring of filename to see if it can be written
         overwrite = args.overwrite
-        newfilepath = "{DP}/{PROF}.py".format(PROF=newprofile,DP=dirpath)
+        newfilepath = "{DP}/{PROF}.py".format(PROF=newprofile, DP=dirpath)
 
         if overwrite is not True:
             if os.path.isfile(newfilepath):
                 print 'here'
-                raise ValueError('Already Exists, use -o or --overwrite to overwrite file')
-        os.rename(oldfilepath,newfilepath)
+                raise ValueError(
+                    'Already Exists, use -o or --overwrite to overwrite file')
+        os.rename(oldfilepath, newfilepath)
 
-            
-        
+
 # In order to actually use these magics, you must register them with a
 # running IPython.  This code must be placed in a file that is loaded once
 # IPython is up and running:
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+major = 0
+minor = 1
+patch = 0
+version = '.'.join([str(v) for v in [major, minor, patch]])
+
+
+setup(name='boilermagic',
+      version=version,
+      description='Magic functions to load boilerplate code',
+      url='https://github.com/pm8k/boilermagic',
+      download_url = 'https://github.com/pm8k/boilermagic/archive/0.1.tar.gz', # I'll explain this in a second
+
+      long_description=readme(),
+      author='Matthew Russell',
+      author_email='astromars42@gmail.com',
+      license='MIT',
+      keywords=['python', 'magic', 'jupyter', 'ipython', 'boiler', 'plate'],
+
+      packages=['boilermagic'],
+        install_requires=[
+          'ipython'
+      ],
+      include_package_data=True,
+
+      zip_safe=False)
